8d7b22faa72a1cc56cff13e3379e44b2fecf1371
rheaanne55 <rheajacob25@gmail.com>
2026-07-11 15:09:21 -0400

    Refactor popup.js to remove camera functionality
    Removed camera initialization and message handling logic.

--- popup.js
+++ popup.js
@@ -1,25 +1,8 @@
-<video id="camera" autoplay playsinline></video>
+<!DOCTYPE html>
+<html>
+  <body>
+    <video id="camera" autoplay playsinline></video>
+    <script src="popup.js"></script>
+  </body>
+</html>
 
-const video = document.getElementById("camera");
-
-navigator.mediaDevices.getUserMedia({ video: true })
-  .then(stream => {
-    video.srcObject = stream;
-  })
-  .catch(err => {
-    console.error("Camera error:", err);
-  });
-
-chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-  chrome.tabs.sendMessage(tabs[0].id, {
-    type: "CAMERA_READY",
-    data: "Camera stream started"
-  });
-});
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === "CAMERA_READY") {
-    console.log("Popup says:", message.data);
-    // You can trigger your fitness logic here
-  }
-});
